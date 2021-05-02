be2290e2964937e2189926b714cd0f905bae76de
null223 <sumidamao@gmail.com>
2021-05-02 15:51:20 +0900

    fix error

--- line/views/tools/error.py
+++ line/views/tools/error.py
@@ -1,9 +1,13 @@
+from linebot.models import TextSendMessage
 from .line_bot import line_bot_api
-from .json import reply_token
+from .json import reply_token, line_id
 from .rich_menu import remove_rich_menu
+from line.models import RoomMember
 
 def SomeError(event):
+    member = RoomMember.objects.line().filter(line_id=line_id(event))
     if member.exists():
+        member = member.latest()
         member.status = 'ended'
         member.save()
         if not member.room.member_set.all().exclude(status='ended').exists():
